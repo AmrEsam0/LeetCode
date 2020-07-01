9061a0a32761a77d0ae16c9a8bd5475e0bba5571
Amr Essam <amr_esam@ymail.com>
2020-07-01 22:31:11 +0200

    He''s happy, I'm not

--- 202.happy-number.cpp
+++ 202.happy-number.cpp
@@ -10,6 +10,25 @@ using namespace std;
 
 class Solution {
    public:
-    bool isHappy(int n) {}
+    bool isHappy(int n) {
+        set<int> s;
+        while (1) {
+            int sum = 0;
+            while (n != 0) {
+                sum += pow(n % 10, 2);
+                n = n / 10;
+            }
+            if (sum == 1) {
+                return 1;
+            }
+            if (s.find(sum) == s.end()) {
+                s.insert(sum);
+            } else {
+                return 0;
+            }
+            n = sum;
+        }
+        return 0;
+    }
 };
 // @lc code=end
